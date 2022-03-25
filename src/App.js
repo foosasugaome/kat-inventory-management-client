2e540b7fcfc547956c44bb167e3e229fb670d5e4
justin-aquino <justin.aquino25@gmail.com>
2022-03-25 10:34:43 -0700

    added routes on App.js and Browser router on index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom"
+import About from './components/pages/About'
+// import InventoryList from ".components/pages/InventoryList"
+import Navibar from "./components/layout/Navibar"
+import Footer from "./components/layout/Footer"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        {/* <Route path="" element={<InventoryList />} /> */}
+      </Routes>
     </div>
   );
 }
