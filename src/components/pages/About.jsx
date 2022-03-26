cff435a22d42a6e0fac2e24bba26256766d34e22
Norman Teodoro <normanteodoro@hotmail.com>
2022-03-26 10:44:28 -0700

    added a little styling

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <h4>A reliable software for all your inventory needs.</h4>
             <div className='card-profile'>
         
-            <img src="https://media-exp1.licdn.com/dms/image/D5603AQGQu8VBjvv-9g/profile-displayphoto-shrink_800_800/0/1632159592194?e=2147483647&v=beta&t=LK4l2e2Xe_j4QwYjhR1JUMJliZiyiY7dfVTkN65mMrA" alt="" />
+            <img src="tkim.jpg" alt="" />
            
             <h3>Tae Kim</h3>
             <h4>Programmer I</h4>
@@ -17,14 +17,14 @@ const About = () => {
            
            
         </div>
-        <div>
-            <img src="https://media-exp1.licdn.com/dms/image/C5603AQExxnG58-BJfQ/profile-displayphoto-shrink_800_800/0/1644453578742?e=1653523200&v=beta&t=P8zMAHCbIi516e1QcBtJ6LfBA20gbVqk7MNCAQqg9Gc" alt="" />
+        <div className='card-profile'>
+            <img src="jaquino.jpg" alt="" />
             <h3>Justin Aquino</h3>
             <h4>Programmer I</h4>
             <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quisquam! Modi iste maiores dolor delectus quis, esse fugiat! Dicta voluptate magnam voluptatibus consequuntur expedita ullam maxime explicabo, obcaecati dolorem iste!</h5>
         </div>
-        <div>
-            <img src="https://media-exp1.licdn.com/dms/image/C5603AQEKqWyy7-PFcg/profile-displayphoto-shrink_800_800/0/1646895664433?e=1653523200&v=beta&t=IWZF1qi5zI9OupOZSLqicbhxaaGAWuA95F36PJoPqcg" alt="" />
+        <div className='card-profile'>
+            <img src="nteodoro.jpg" alt="" />
             <h3>Norman Teodoro</h3>
             <h4>Git Manager</h4>
             <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quisquam! Modi iste maiores dolor delectus quis, esse fugiat! Dicta voluptate magnam voluptatibus consequuntur expedita ullam maxime explicabo, obcaecati dolorem iste!</h5>
